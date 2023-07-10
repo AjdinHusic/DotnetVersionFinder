0e60d52c4fc564ae421d33b7d38e0e3de4487503
Ajdin Husic <ajdin.husic.dev@gmail.com>
2023-07-10 15:13:57 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
-const { readFileSync } = require("fs");
+const { readFileSync, readdirSync } = require("fs");
 const { parseString } = require("xml2js");
 
 try {
     const path = core.getInput("path");
 
+    // Log current working directory
+    console.log("Current working directory:", process.cwd());
+
+    // Log files and folders in the current working directory
+    console.log("Files and folders in the current working directory:", readdirSync(process.cwd()).join(', '));
+    
+    
     const file = readFileSync(path);
 
     parseString(file, (err, result) => {
